a77b3e2b60c9bc91fe3ebf39ee813c6b8d0f4501
MthsAmaral <mateusamaral907@gmail.com>
2026-02-11 21:51:18 -0300

    Adicionar arquivos de projeto.

---
+++ ProcessamentoImagens/frmPrincipal.designer.cs
@@ -0,0 +1,138 @@
+﻿namespace ProcessamentoImagens
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictBoxImg1 = new System.Windows.Forms.PictureBox();
+            this.pictBoxImg2 = new System.Windows.Forms.PictureBox();
+            this.btnAbrirImagem = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnLuminanciaComDMA = new System.Windows.Forms.Button();
+            this.btnNegativoComDMA = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxImg1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxImg2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictBoxImg1
+            // 
+            this.pictBoxImg1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictBoxImg1.Location = new System.Drawing.Point(7, 7);
+            this.pictBoxImg1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictBoxImg1.Name = "pictBoxImg1";
+            this.pictBoxImg1.Size = new System.Drawing.Size(800, 615);
+            this.pictBoxImg1.TabIndex = 102;
+            this.pictBoxImg1.TabStop = false;
+            // 
+            // pictBoxImg2
+            // 
+            this.pictBoxImg2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictBoxImg2.Location = new System.Drawing.Point(815, 7);
+            this.pictBoxImg2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictBoxImg2.Name = "pictBoxImg2";
+            this.pictBoxImg2.Size = new System.Drawing.Size(800, 615);
+            this.pictBoxImg2.TabIndex = 105;
+            this.pictBoxImg2.TabStop = false;
+            // 
+            // btnAbrirImagem
+            // 
+            this.btnAbrirImagem.Location = new System.Drawing.Point(7, 630);
+            this.btnAbrirImagem.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAbrirImagem.Name = "btnAbrirImagem";
+            this.btnAbrirImagem.Size = new System.Drawing.Size(135, 28);
+            this.btnAbrirImagem.TabIndex = 106;
+            this.btnAbrirImagem.Text = "Abrir Imagem";
+            this.btnAbrirImagem.UseVisualStyleBackColor = true;
+            this.btnAbrirImagem.Click += new System.EventHandler(this.btnAbrirImagem_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(149, 630);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(135, 28);
+            this.btnLimpar.TabIndex = 107;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnLuminanciaComDMA
+            // 
+            this.btnLuminanciaComDMA.Location = new System.Drawing.Point(416, 654);
+            this.btnLuminanciaComDMA.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLuminanciaComDMA.Name = "btnLuminanciaComDMA";
+            this.btnLuminanciaComDMA.Size = new System.Drawing.Size(132, 28);
+            this.btnLuminanciaComDMA.TabIndex = 109;
+            this.btnLuminanciaComDMA.Text = "Luminância";
+            this.btnLuminanciaComDMA.UseVisualStyleBackColor = true;
+            this.btnLuminanciaComDMA.Click += new System.EventHandler(this.btnLuminanciaComDMA_Click);
+            // 
+            // btnNegativoComDMA
+            // 
+            this.btnNegativoComDMA.Location = new System.Drawing.Point(416, 690);
+            this.btnNegativoComDMA.Margin = new System.Windows.Forms.Padding(4);
+            this.btnNegativoComDMA.Name = "btnNegativoComDMA";
+            this.btnNegativoComDMA.Size = new System.Drawing.Size(132, 28);
+            this.btnNegativoComDMA.TabIndex = 111;
+            this.btnNegativoComDMA.Text = "Negativo";
+            this.btnNegativoComDMA.UseVisualStyleBackColor = true;
+            this.btnNegativoComDMA.Click += new System.EventHandler(this.btnNegativoComDMA_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1627, 748);
+            this.Controls.Add(this.btnNegativoComDMA);
+            this.Controls.Add(this.btnLuminanciaComDMA);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnAbrirImagem);
+            this.Controls.Add(this.pictBoxImg2);
+            this.Controls.Add(this.pictBoxImg1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Formulário Principal";
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxImg1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxImg2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictBoxImg1;
+        private System.Windows.Forms.PictureBox pictBoxImg2;
+        private System.Windows.Forms.Button btnAbrirImagem;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnLuminanciaComDMA;
+        private System.Windows.Forms.Button btnNegativoComDMA;
+    }
+}
+
